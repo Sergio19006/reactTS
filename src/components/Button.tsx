08429cc3f828c98263e8e204d2f386c60ec40aa9
sergio19006 <sergiodphn@gmail.com>
2019-07-12 17:21:09 +0100

    Mejorando el bind()

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,6 +10,7 @@ export default class CounterButton extends React.Component<{}, CounterButtonStat
      this.state = {
        numberOfClicks: 0
       }
+    this.incrementCount = this.incrementCount.bind(this);
   }
   incrementCount() {
     this.setState({ 
@@ -17,6 +18,6 @@ export default class CounterButton extends React.Component<{}, CounterButtonStat
     });
   }
   render() {
-    return <button onClick={this.incrementCount.bind(this)}>{this.state.numberOfClicks}</button>;
+    return <button onClick={this.incrementCount}>{this.state.numberOfClicks}</button>;
   }
 }
